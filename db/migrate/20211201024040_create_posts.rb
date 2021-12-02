fbaf8a12d1a446c673a4bbbb28ea4387c6a8dda9
Doug Jones <DSJR1284@GMAIL.COM>
2021-12-01 22:35:41 -0500

    set up models and database

--- db/migrate/20211201024040_create_posts.rb
+++ db/migrate/20211201024040_create_posts.rb
@@ -1,7 +1,11 @@
 class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
-
+      t.string :topic
+      t.string :title 
+      t.text :description
+      t.string :image
+      t.integer :rating
       t.timestamps
     end
   end
--- db/migrate/20211201024051_create_comments.rb
+++ db/migrate/20211201024051_create_comments.rb
@@ -1,7 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.1]
   def change
     create_table :comments do |t|
-
+      t.text :opinion 
+      t.integer :rating
+      t.integer :post_id
       t.timestamps
     end
   end
