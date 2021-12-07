3cee86a422a09b81aaee4a589b427a5dbf5698aa
Doug Jones <DSJR1284@GMAIL.COM>
2021-12-07 07:06:20 -0500

    created user for seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User Data 
+LoCodes = User.create(username: 'LoCodes', password: 'abcdefg')
+CodeKel = User.create(username: 'CodeKel', password: 'mycodes')
+
