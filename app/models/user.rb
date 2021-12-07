bd2628ff10c039b9565b8ceb1bb835883864d9d0
Doug Jones <DSJR1284@GMAIL.COM>
2021-12-06 20:55:45 -0500

    add user_id to post and comment tables

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
     has_many :posts 
     has_many :comments 
+    has_secure_password
+    validates :username, presence: true, :uniqueness => { :case_sensitive => false }
+    validates :password, length: { minimum: 6 }
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_01_024051) do
+ActiveRecord::Schema.define(version: 2021_12_07_015425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2021_12_01_024051) do
     t.integer "post_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
@@ -29,6 +30,14 @@ ActiveRecord::Schema.define(version: 2021_12_01_024051) do
     t.text "description"
     t.string "image"
     t.integer "rating"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
